e4d411646d91303e78f4aa8d642e41cceca8720c
Hanna Dozhyna <annadozhyna@gmail.com>
2019-10-02 16:53:47 +0300

    add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function multiply(first, second) {
-  // your solution
-}
+  let production = [];
+  let firstNumber = first.split("").reverse();
+  let secondNumber = second.split("").reverse();
+  for (let i = 0; i < firstNumber.length; i++) {
+    for (let j = 0; j < secondNumber.length; j++) {
+      let sum = firstNumber[i] * secondNumber[j];
+      production[i + j] = production[i + j] ? production[i + j] + sum : sum;
+    }
+  }
+  for (let i = 0; i < production.length; i++) {
+    let number = production[i] % 10;
+    let transfer = Math.floor(production[i] / 10);
+    production[i] = number;
+
+    if (production[i + 1]) production[i + 1] += transfer;
+    else if (transfer != 0) production[i + 1] = transfer;
+  }
+  let result = production.reverse().join("");
+  return result;
+};
